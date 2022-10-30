734e4c03af4524ddb453f1e5b1ba9a12cd85e3d9
NotZeetaa <rodrigo2005contente@gmail.com>
2022-10-30 13:23:20 +0000

    anykernel: Adapt to apollo
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -10,8 +10,9 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=lmi
-device.name2=lmipro
+device.name1=apollo
+device.name2=apolloin
+device.name3=apollon
 supported.versions=11 - 13
 supported.patchlevels=
 '; } # end properties
@@ -32,37 +33,6 @@ patch_vbmeta_flag=auto;
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-case "$ZIPFILE" in
-  *66fps*|*66hz*)
-    ui_print "  • Setting 66 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
-    ;;
-  *69fps*|*69hz*)
-    ui_print "  • Setting 69 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
-    ;;
-  *72fps*|*72hz*)
-    ui_print "  • Setting 72 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
-    ;;
-  *77fps*|*77hz*)
-    ui_print "  • Setting 77 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
-    ;;
-  *79fps*|*79hz*)
-    ui_print "  • Setting 79 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=5"
-    ;;
-  *80fps*|*80hz*)
-    ui_print "  • Setting 80 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=6"
-    ;;
-  *)
-    ui_print "  • Setting 60 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" ""
-    ;;
-esac
 
 ## AnyKernel boot install
 dump_boot;
